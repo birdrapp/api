cb8dc6a38ca6fe50ece6588c05e7ddeb9a0ea010
Matthew Williams <matthew.williams@performgroup.com>
2017-03-04 14:53:43 +0000

    Create start script using cluster

---
+++ index.js
@@ -0,0 +1,21 @@
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+  var cpuCount = require('os').cpus().length;
+
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+} else {
+  const server = require('./server');
+  const port = 7080;
+  server.listen(port);
+  console.log(`Server listening on ${port}`);
+}
+
+cluster.on('exit', function (worker) {
+  // Replace the dead worker,
+  // we're not sentimental
+  console.log('Process %d died', worker.id);
+  cluster.fork();
+});
