6433b345bf27f0ff87eed9f5e75702db983b7352
Matthew Williams <m.williams@me.com>
2017-03-08 16:04:50 +0000

    Add CORS support

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const app = express();
 // Allow POSTing of JSON data
 app.use(bodyParser.json());
 
+// Enable CORS requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/v1/birds', birdsRoute);
 
 // 404 handler
---
+++ test/server.js
@@ -0,0 +1,12 @@
+const app = require('../server');
+const assert = require('assert');
+const request = require('supertest');
+
+describe('Server', () => {
+  it('supports CORS', async () => {
+    await request(app)
+      .get('/')
+      .expect('Access-Control-Allow-Origin', '*')
+      .expect('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  });
+});
